5335d997fb8a22fc6a105d97d14ec5fb03a15a5b
Gabriele-P03 <pacegabriele@libero.it>
2024-11-19 23:05:23 +0100

    Sync

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
-let x = 0
-window.onload = function() {
-    while(x < 50){
-        x++;
-        console.log(x)
-        document.getElementById("img_striscione").transform= 'translate(${x}px, 0px)' ; 
+window.onload = function(){
+
+    let frasi_c = document.getElementsByTagName("li").length
+
+    for(let i = 0; i < frasi_c; i++){
+        let nomeLI = "li_aside_frasi_" + (i+1)
+        console.log(nomeLI)
+        let li = document.getElementById(nomeLI);
+        if(i%2 == 1){
+            li.style.textAlign = "right";
+            li.animate(
+                [
+                    {
+                        transform: 'translateX(-100%)'
+                    },
+                    {
+                        transform: 'translateX(0%)'
+                    }
+                ],{
+                    duration: 1000,
+                    iterations: 1
+                }
+            )
+        }else{
+            li.style.textAlign = "left";
+            li.animate(
+                [
+                    {
+                        transform: 'translateX(100%)'
+                    },
+                    {
+                        transform: 'translateX(0%)'
+                    }
+                ],{
+                    duration: 1000,
+                    iterations: 1
+                }
+            )
+        }
+
     }
 }
