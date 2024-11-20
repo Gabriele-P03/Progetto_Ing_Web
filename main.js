2ae182ae6dda0180b67324484da246b07f6af33a
Gabriele-P03 <pacegabriele@libero.it>
2024-11-20 16:10:26 +0100

    Sync

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ window.onload = function(){
 
     for(let i = 0; i < frasi_c; i++){
         let nomeLI = "li_aside_frasi_" + (i+1)
-        console.log(nomeLI)
         let li = document.getElementById(nomeLI);
         if(i%2 == 1){
             li.style.textAlign = "right";
@@ -39,4 +38,37 @@ window.onload = function(){
         }
 
     }
+}
+
+
+let footer_arrow_state = true
+function footer_up(){
+
+    let arrow = document.getElementById("footer_arrow");
+    let footer = document.getElementById("footer_info");
+
+    if(footer_arrow_state){
+        arrow.setAttribute
+        arrow.setAttribute("src", 'resources/body/footer/footer_down.png');
+        footer_arrow_state = false;
+        footer.style.display = "block";
+        footer.animate([
+                {
+                    transform: 'translateY(100%)'
+                },
+                {
+                    transform: 'translateY(0%)'
+                }
+            ],{
+                duration: 1000,
+                iterations: 1
+            }
+        );
+    }else{
+        arrow.setAttribute("src", 'resources/body/footer/footer_up.png');
+        footer_arrow_state = true;
+        footer.style.display = "none";
+    }
+    console.log(footer);
+    console.log(footer_arrow_state);
 }
